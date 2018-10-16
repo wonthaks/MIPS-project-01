e65c822b1eac48de6db5f0a616cafd8f909e9d3f
Wontha <wontha.ks@gmail.com>
2018-10-15 21:38:05 -0400

    Added code to print out integer 0

--- MIPS01.s
+++ MIPS01.s
@@ -5,5 +5,9 @@ main:
 	la $a0, '@'			#print out the @ symbol
 	syscall
 	
+	li $v0, 1			#li 1 to print out integers
+	addi $a0, $a0, -64	#print out the integer 0
+	syscall
+	
 	li $v0, 10			#to end the program
 	syscall
